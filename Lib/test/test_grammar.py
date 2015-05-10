03430d6bf41e06052829d7575c734de19481ce88
Yury Selivanov <yselivanov@sprymix.com>
2015-05-10 17:03:22 -0400

    async: More tests in test_grammar

--- Lib/test/test_grammar.py
+++ Lib/test/test_grammar.py
@@ -2,6 +2,7 @@
 # This just tests whether the parser accepts them all.
 
 from test.support import check_syntax_error
+import inspect
 import unittest
 import sys
 # testing import *
@@ -1045,7 +1046,7 @@ class GrammarTests(unittest.TestCase):
         async()
         self.assertEqual(await, 10)
 
-        self.assertFalse(bool(async.__code__.co_flags & 0x80))
+        self.assertFalse(bool(async.__code__.co_flags & inspect.CO_COROUTINE))
 
         async def test():
             def sum():
@@ -1057,7 +1058,7 @@ class GrammarTests(unittest.TestCase):
                 await someobj()
 
         self.assertEqual(test.__name__, 'test')
-        self.assertTrue(bool(test.__code__.co_flags & 0x80))
+        self.assertTrue(bool(test.__code__.co_flags & inspect.CO_COROUTINE))
 
         def decorator(func):
             setattr(func, '_marked', True)
@@ -1068,7 +1069,57 @@ class GrammarTests(unittest.TestCase):
             return 22
         self.assertTrue(test2._marked)
         self.assertEqual(test2.__name__, 'test2')
-        self.assertTrue(bool(test2.__code__.co_flags & 0x80))
+        self.assertTrue(bool(test2.__code__.co_flags & inspect.CO_COROUTINE))
+
+    def test_async_for(self):
+        class Done(Exception): pass
+
+        class AIter:
+            async def __aiter__(self):
+                return self
+            async def __anext__(self):
+                raise StopAsyncIteration
+
+        async def foo():
+            async for i in AIter():
+                pass
+            async for i, j in AIter():
+                pass
+            async for i in AIter():
+                pass
+            else:
+                pass
+            raise Done
+
+        with self.assertRaises(Done):
+            foo().send(None)
+
+    def test_async_with(self):
+        class Done(Exception): pass
+
+        class manager:
+            async def __aenter__(self):
+                return (1, 2)
+            async def __aexit__(self, *exc):
+                return False
+
+        async def foo():
+            async with manager():
+                pass
+            async with manager() as x:
+                pass
+            async with manager() as (x, y):
+                pass
+            async with manager(), manager():
+                pass
+            async with manager() as x, manager() as y:
+                pass
+            async with manager() as x, manager():
+                pass
+            raise Done
+
+        with self.assertRaises(Done):
+            foo().send(None)
 
 
 if __name__ == '__main__':
